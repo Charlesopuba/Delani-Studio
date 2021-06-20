989ae32f80dd49a55c2e42ff78ac95a89fbd21a1
Charlesopuba <charles.opuba@student.moringaschool.com>
2021-06-20 20:09:57 +0300

    js functions

--- static/js/main.js
+++ static/js/main.js
@@ -1,75 +1,29 @@
-$(function () {
-    let desp = $('#desp');
-    let devp = $('#devp');
-    let prop = $('#prop');
-    let destext = $('#destxt');
-    let devtext = $('#devtext');
-    let protext = $('#protext');
+$(document).ready(function(){
+    
+    $('.main').click(function () {
+        $(this).children('.hideMe').slideToggle(1200);
+        $(this).children('.show').toggle(1500);
+      });
 
-    $(desp).hide();
-    $(devp).hide();
-    $(prop).hide();
+    $('.card').hover(function(event){
+      $(".card").hover(function () {
+        $(this).children(".card-1").fadeToggle('fast', "linear");
+      });
+      });
+      event.preventDefault();
+    
 
-    //Show paragraph content
-    $('.des').on('click', function () {
-        $('.des').slideUp('slow');
-        $(desp).show(500);
-        $(destext).animate({fontWeight:'bolder',fontSize:'30px'})
-
+    $('form#myForm').submit(function(){
+      var name = $('#name').val();
+      var pass = $('#email').val();
+      var mess = $('#mess').val();
+      alert("Hi " +name+ " we have received your message and we will get in touch. Thank you for contacting us.");
     });
-    $('.dev').on('click', function () {
-        $('.dev').slideUp('slow');
-        $(devp).show(500);
-        $(devtext).animate({fontWeight:'bolder',fontSize:'30px'})
-    });
-    $('.pro').on('click', function () {
-        $('.pro').slideUp('slow');
-        $(prop).show(500);
-        $(protext).animate({fontWeight:'bolder',fontSize:'30px'})
-    });
-
-    //Hide paragraph content
-    $(desp, destext).on('click', function () {
-        $('.des').slideDown();
-        $(desp).hide(500);
-        $(destext).animate({fontWeight:'normal',fontSize:'16px'})
-    });
-    $(devp, devtext).on('click', function () {
-        $('.dev').slideDown();
-        $(devp).hide(500);
-        $(devtext).animate({fontWeight:'normal',fontSize:'16px'})
-    });
-    $(prop, protext).on('click', function () {
-        $('.pro').slideDown();
-        $(prop).hide(500);
-        $(protext).animate({fontWeight:'normal',fontSize:'16px'})
-    })
-
-    //Overlay images
-
-    $('.portimg').hover(function(){
-        $('.porttext',this).slideToggle('slow');
-     }, function(){
-        $('.porttext',this).slideToggle('slow');
-     });
-
-
-    //Form Validation
-        $("form#form").on('submit',function(event){
-            event.preventDefault();
-            let name = $("input#name").val();
-            let email = $("input#email").val();
-            let message = $("textarea#message").val();
-
-            if ($("input#name").val() && $("input#email").val()){
-                alert ("Hey " + name + ", Thanks for reaching out, we'll get back to you shortly...cheers");
-            }
-            else {
-                alert("Please provide your correct name and email!");
-            }
-
-        });
-
-
-
+    
+    $("button").on('click', function(){
+      $('form').each(function(){
+        this.reset();
+      });
+    
+});
 });
